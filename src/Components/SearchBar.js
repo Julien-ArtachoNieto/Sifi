d973420a45080616a3cedb777a9c4ee6b0befa6a
Julien <julien.artachonieto@gmail.com>
2022-01-22 16:52:25 +0100

    Initialisation du projet REACT/Creation du composant searchBar

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,14 @@
+const SearchBar =({search,setSearch,...props})=>{
+    const handleClick=()=>{
+        const input = document.querySelector('.searchBar')
+        setSearch(input.value)
+    }
+    return (
+    <div>
+        <h1>Sifi</h1>
+        <input className='searchBar'/>
+        <button className='searchBar-btn' onClick={handleClick}>Search It</button>
+    </div>
+)
+}
+export default SearchBar;
