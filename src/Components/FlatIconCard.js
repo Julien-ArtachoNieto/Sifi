5a51498101bf0d2001c1ba151deadbd19f156a81
Julien <julien.artachonieto@gmail.com>
2022-01-25 10:01:00 +0100

    Mise en place de la récupération d'un token lors de l'appel  à l'api Flaticon

--- src/Components/FlatIconCard.js
+++ src/Components/FlatIconCard.js
@@ -3,16 +3,22 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 const FlatIconCard =({search})=>{
-    const apiKey=process.env.REACT_APP_API_KEY_FLATICON
+    const APIKey=process.env.REACT_APP_API_KEY_FLATICON
     const [results,setResults]=useState('')
+    const [auth,setAuth]=useState('')
 
     useEffect(()=>{
-        axios.post(`https://api.flaticon.com/v3/app/authentication\`,{apiKey:apiKey})
-        .then(response=>console.log('REPONSE POST',response)
-        
+        axios.post(`https://api.flaticon.com/v3/app/authentication`,{apikey:APIKey})
+        .then(response=> setAuth(response.data))
+        if(auth!=''){
+         axios.get(`https://api.flaticon.com/v3/search/icons/`,{Authorization:auth.data.token,q:search})
+            .then(response=>console.log('REPONSE GET', response))
+        }
+        console.log('VALEUR TOKEN',auth.data.token)
     },[search])
     return (
         <div className='cards'>
+
             <a className='header-cards' href="https://www.flaticon.com/">
                 <img src="https://media.flaticon.com/dist/min/img/logo/flaticon_negative.svg" alt='Flaticon Logo'/>
             </a>
